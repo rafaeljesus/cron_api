2aedabe39b236510736cb71343139f6abd9d7cb6
Rafael Jesus <rafaelljesus86@gmail.com>
2016-06-03 23:29:29 -0300

    Split specs

--- test/cron/scheduler_test.exs
+++ test/cron/scheduler_test.exs
@@ -5,6 +5,12 @@ defmodule Cron.SearchTest do
   test "should add job" do
     with event <- %Event{id: 1, cron: '* * * * *'},
       {:ok, job_id} <- Scheduler.add(event),
+      do: assert job_id != nil
+  end
+
+  test "should find job" do
+    with event <- %Event{id: 1, cron: '* * * * *'},
+      {:ok, job_id} <- Scheduler.add(event),
       {:ok, job} <- Scheduler.find(event.id),
       do: assert job.state == 'active'
   end
